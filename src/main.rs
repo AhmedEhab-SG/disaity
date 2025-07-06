9dd5e9389a4461937e37b22550c2d24e8efe7dd7
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2025-07-06 13:08:30 +0300

    init the lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::{
+    Client,
+    all::{Context, EventHandler, GatewayIntents, Message, Ready},
+    async_trait,
+};
+use tokio::main;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    // Set a handler for the `message` event. This is called whenever a new message is received.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple events can be
+    // dispatched simultaneously.
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            // Sending a message can fail, due to a network error, an authentication error, or lack
+            // of permissions to post in the channel, so log to stdout when some error happens,
+            // with a description of it.
+            if let Err(e) = msg.channel_id.broadcast_typing(&ctx.http).await {
+                println!("Error faild toy typing:{e:?}");
+            };
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a shard is booted, and
+    // a READY payload is sent by Discord. This payload contains data like the current user's guild
+    // Ids, current user data, private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[main]
+async fn main() {
+    let token = dotenv::var("CLIENT_TOKEN").unwrap();
+
+    let intents = GatewayIntents::GUILDS
+        | GatewayIntents::GUILD_MEMBERS
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_VOICE_STATES
+        | GatewayIntents::GUILD_INTEGRATIONS
+        | GatewayIntents::DIRECT_MESSAGES;
+
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(e) = client.start().await {
+        println!("Client error: {e:?}");
+    }
 }
