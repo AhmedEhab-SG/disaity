304e25e66026732bd9a973bee92afd99e1b767fe
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2025-11-20 12:16:30 +0200

    update dep

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,55 @@
-use serenity::{
-    Client,
-    all::{Context, EventHandler, GatewayIntents, Message, Ready},
-    async_trait,
+use ::serenity::all::GatewayIntents;
+use poise::{
+    Framework, FrameworkOptions, PrefixFrameworkOptions, builtins, command,
+    serenity_prelude as serenity,
 };
 use tokio::main;
 
-struct Handler;
+struct Data {}
 
-#[async_trait]
-impl EventHandler for Handler {
-    // Set a handler for the `message` event. This is called whenever a new message is received.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple events can be
-    // dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an authentication error, or lack
-            // of permissions to post in the channel, so log to stdout when some error happens,
-            // with a description of it.
-            if let Err(e) = msg.channel_id.broadcast_typing(&ctx.http).await {
-                println!("Error faild toy typing:{e:?}");
-            };
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
-            }
-        }
-    }
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-    // Set a handler to be called on the `ready` event. This is called when a shard is booted, and
-    // a READY payload is sent by Discord. This payload contains data like the current user's guild
-    // Ids, current user data, private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
+/// Displays your or another user's account creation date
+#[command(slash_command, prefix_command, rename = "play", aliases("p"))]
+async fn age(ctx: Context<'_>, #[rest] query: String) -> Result<(), Error> {
+    ctx.say(format!("Searching for {query}")).await?;
+    Ok(())
 }
 
 #[main]
 async fn main() {
     let token = dotenv::var("CLIENT_TOKEN").unwrap();
 
-    let intents = GatewayIntents::GUILDS
+    let intents = GatewayIntents::non_privileged()
+        | GatewayIntents::GUILDS
         | GatewayIntents::GUILD_MEMBERS
         | GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT
         | GatewayIntents::GUILD_VOICE_STATES
         | GatewayIntents::GUILD_INTEGRATIONS
+        | GatewayIntents::MESSAGE_CONTENT
         | GatewayIntents::DIRECT_MESSAGES;
 
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Error creating client");
+    let framework = Framework::builder()
+        .options(FrameworkOptions {
+            commands: vec![age()],
+            prefix_options: PrefixFrameworkOptions {
+                prefix: Some("!".into()),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
 
-    if let Err(e) = client.start().await {
-        println!("Client error: {e:?}");
-    }
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap();
 }
