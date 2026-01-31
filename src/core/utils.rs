2e40c007a015fbaee8b1dc36b7eb007e462d8c14
ahmed <ahmedehab.sg@gmail.com>
2026-02-01 01:17:20 +0200

    adj args

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -3,35 +3,40 @@ pub fn load_bin() {
     {
         use std::env;
 
-        if let Ok(mut exe_path) = std::env::current_exe() {
-            exe_path.pop();
-            let bin_path = exe_path.join("bin");
+        let project_bin = env::current_dir().unwrap().join("bin");
 
-            if let Some(path) = env::var_os("PATH") {
-                let mut paths = env::split_paths(&path).collect::<Vec<_>>();
-                paths.insert(0, bin_path);
-                let new_path = env::join_paths(paths).unwrap();
+        let mut exe_bin = env::current_exe().unwrap();
+        exe_bin.pop();
+        let exe_bin = exe_bin.join("bin");
 
-                unsafe {
-                    env::set_var("PATH", &new_path);
-                }
+        if let Some(path) = env::var_os("PATH") {
+            let mut paths = env::split_paths(&path).collect::<Vec<_>>();
+
+            paths.insert(0, exe_bin);
+            paths.insert(0, project_bin);
+
+            let new_path = env::join_paths(paths).unwrap();
+            unsafe {
+                env::set_var("PATH", &new_path);
             }
         }
     }
 
-    let check = std::process::Command::new("ffmpeg")
-        .arg("-version")
-        .output();
-    match check {
-        Ok(_) => println!("✅ FFmpeg is ready to go!"),
-        Err(_) => println!("❌ FFmpeg NOT FOUND. Install it or check /bin folder."),
-    }
+    {
+        let check = std::process::Command::new("ffmpeg")
+            .arg("-version")
+            .output();
+        match check {
+            Ok(_) => println!("✅ FFmpeg is ready to go!"),
+            Err(_) => println!("❌ FFmpeg NOT FOUND. Install it or check /bin folder."),
+        }
 
-    let check_yt = std::process::Command::new("yt-dlp")
-        .arg("-version")
-        .output();
-    match check_yt {
-        Ok(_) => println!("✅ yt-dlp is ready to go!"),
-        Err(_) => println!("❌ yt-dlp NOT FOUND. Install it or check /bin folder."),
+        let check_yt = std::process::Command::new("yt-dlp")
+            .arg("-version")
+            .output();
+        match check_yt {
+            Ok(_) => println!("✅ yt-dlp is ready to go!"),
+            Err(_) => println!("❌ yt-dlp NOT FOUND. Install it or check /bin folder."),
+        }
     }
 }
