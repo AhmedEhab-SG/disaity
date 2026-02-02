9965936873190493e757be31cfd26fedc92e02f8
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2026-02-02 17:35:53 +0200

    update dep

--- src/commands/play.rs
+++ src/commands/play.rs
@@ -1,4 +1,7 @@
-use poise::command;
+use poise::{
+    command,
+    serenity_prelude::{CreateEmbed, CreateEmbedFooter},
+};
 use songbird::input::YoutubeDl;
 
 use crate::core::{Context, Error};
@@ -37,16 +40,18 @@ pub async fn play(ctx: Context<'_>, #[rest] query: String) -> Result<(), Error> 
     let handler_lock = manager.join(guild_id, channel_id).await?;
     let mut handler = handler_lock.lock().await;
 
+    let defer_msg = ctx.say("🔎 Searching...").await?;
+
     let user_args = vec![
-        "--extractor-args".to_string(),
-        "youtube:player_client=android,skip=webpage".to_string(),
-        "-f".to_string(),
-        "bestaudio[ext=webm]/bestaudio/best".to_string(),
+        // "--extractor-args".to_string(),
+        // "youtube:player_client=android,skip=webpage".to_string(),
+        // "-f".to_string(),
+        // "bestaudio[ext=webm]/bestaudio/best".to_string(),
     ];
 
     let client = reqwest::Client::new();
 
-    let source = if do_search {
+    let mut src: songbird::input::Input = if do_search {
         YoutubeDl::new_search(client, query)
             .user_args(user_args.clone())
             .into()
@@ -54,9 +59,58 @@ pub async fn play(ctx: Context<'_>, #[rest] query: String) -> Result<(), Error> 
         YoutubeDl::new(client, query).user_args(user_args).into()
     };
 
-    // enqueue (songbird 0.5 uses `enqueue`)
-    handler.enqueue(source).await;
+    let metadata = match src.aux_metadata().await {
+        Ok(m) => m,
+        Err(_e) => {
+            ctx.say("Could not fetch song metadata.").await?;
+            return Ok(());
+        }
+    };
 
-    ctx.say("▶️ Added to queue!").await?;
+    let embed = CreateEmbed::new()
+        .title(metadata.title.as_deref().unwrap_or("Unknown Title"))
+        .url(
+            metadata
+                .source_url
+                .as_deref()
+                .unwrap_or("https://youtube.com"),
+        )
+        .thumbnail(metadata.thumbnail.as_deref().unwrap_or(""))
+        .color(0xFF0000)
+        .field("Duration", format_duration(metadata.duration), true)
+        .field(
+            "Channel",
+            metadata.channel.as_deref().unwrap_or("Unknown"),
+            true,
+        )
+        .footer(CreateEmbedFooter::new(format!(
+            "Requested by {}",
+            ctx.author().name
+        )));
+
+    // enqueue (songbird 0.5 uses `enqueue`)
+    handler.enqueue(src.into()).await;
+
+    defer_msg
+        .edit(
+            ctx,
+            poise::CreateReply::default()
+                .content("▶️ **Added to queue**") // Clear the "Searching" text
+                .embed(embed),
+        )
+        .await?;
+
     Ok(())
+}
+
+fn format_duration(d: Option<std::time::Duration>) -> String {
+    match d {
+        Some(d) => {
+            let seconds = d.as_secs();
+            let minutes = seconds / 60;
+            let rem_seconds = seconds % 60;
+            format!("{:02}:{:02}", minutes, rem_seconds)
+        }
+        None => "Live/Unknown".to_string(),
+    }
 }
