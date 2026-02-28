829af9c6dcca19e7f5737b823e46846f70a140cd
ahmed <ahmedehab.sg@gmail.com>
2026-02-28 03:41:26 +0200

    add cmds

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,4 +1,11 @@
+pub mod idle;
 pub mod playing;
+
+use serenity::all::{ChannelId, GuildId, Http};
+use songbird::{Call, Songbird};
+use std::sync::Arc;
+
+use crate::handlers::{idle::register_idle_timeout, playing::register_playing_info};
 
 #[derive(Debug, Clone)]
 pub struct SongMetadata {
@@ -7,4 +14,19 @@ pub struct SongMetadata {
     pub thumbnail: String,
     pub duration: Option<std::time::Duration>,
     pub request_by: String,
+}
+
+pub async fn register_all(
+    call: &mut Call,
+    guild_id: GuildId,
+    text_channel_id: ChannelId,
+    http: Arc<Http>,
+    manager: Arc<Songbird>,
+) {
+    // Clear default or old handlers to prevent duplicates
+    call.remove_all_global_events();
+
+    register_playing_info(call, text_channel_id, http).await;
+
+    register_idle_timeout(call, guild_id, manager).await;
 }
--- src/uitls/mod.rs
+++ src/uitls/mod.rs
@@ -1,36 +1,4 @@
-use serenity::all::ChannelId;
-use songbird::{Call, Event, TrackEvent};
-use std::{sync::Arc, time::Duration};
-use tokio::sync::Mutex;
-
-use crate::{core::Error, handlers::playing::TrackStartNotifier};
-
-pub async fn get_or_join_voice(
-    manager: &Arc<songbird::Songbird>,
-    guild_id: serenity::all::GuildId,
-    voice_channel_id: ChannelId,
-    text_channel_id: ChannelId,
-    http: Arc<serenity::all::Http>,
-) -> Result<Arc<Mutex<Call>>, Error> {
-    let call = manager.join(guild_id, voice_channel_id).await?;
-
-    let mut handler = call.lock().await;
-
-    // handle that later
-    handler.remove_all_global_events();
-
-    handler.add_global_event(
-        Event::Track(TrackEvent::Play),
-        TrackStartNotifier {
-            channel_id: text_channel_id,
-            http: http.clone(),
-        },
-    );
-
-    drop(handler);
-
-    Ok(call)
-}
+use std::time::Duration;
 
 /// Parse user-supplied timestamp into `Duration`.
 /// Accepts:
